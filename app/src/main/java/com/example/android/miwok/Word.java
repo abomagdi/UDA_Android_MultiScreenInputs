2aa112fc54872f44adde378360a78da3e989f928
Ahmed <a-musa@mail.nissan.co.jp>
2019-06-14 15:12:40 +0900

    Add images and visual polish

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -6,9 +6,23 @@ public class Word {
 
     private String mMiwokTranslation;
 
-    public Word(String defualtTranslation, String MiwokTranslation){
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private int mAudioResourceId;
+    private static final int NO_IMAGE_PROVIDED  = -1;
+
+    public Word(String defualtTranslation, String MiwokTranslation, int audioResource){
         mDefaultTranslation = defualtTranslation;
         mMiwokTranslation = MiwokTranslation;
+        mAudioResourceId = audioResource;
+
+    }
+
+    public Word(String defualtTranslation, String MiwokTranslation, int imageResourceId, int audioResource){
+        mDefaultTranslation = defualtTranslation;
+        mMiwokTranslation = MiwokTranslation;
+        mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResource;
+
     }
 
     public String getDefaultTranslation(){
@@ -19,5 +33,14 @@ public class Word {
         return mMiwokTranslation;
     }
 
+    public int getImageResourceId(){
+        return mImageResourceId;
+    }
+
+    public boolean hasImage(){
+        return mImageResourceId != NO_IMAGE_PROVIDED;
+    }
+
+    public int getAudioResourceId() { return mAudioResourceId; }
 
 }
--- app/src/main/java/com/example/android/miwok/WordAdaptor.java
+++ app/src/main/java/com/example/android/miwok/WordAdaptor.java
@@ -1,20 +1,28 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class WordAdaptor extends ArrayAdapter<Word> {
 
-    public WordAdaptor(Activity context, ArrayList<Word> words){
+    private int backgroudColor;
+    private MediaPlayer mediaPlayer;
+
+    public WordAdaptor(Activity context, ArrayList<Word> words, int bgColor){
         super(context,0,words);
+        backgroudColor = bgColor;
     }
 
     @NonNull
@@ -26,11 +34,39 @@ public class WordAdaptor extends ArrayAdapter<Word> {
                     R.layout.list_item, parent, false);
         }
         Word currentWord = getItem(position);
+
         TextView nameTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
         nameTextView.setText(currentWord.getMiwokTranslation());
+
         TextView nameTextView2 = (TextView) listItemView.findViewById(R.id.default_text_view);
         nameTextView2.setText(currentWord.getDefaultTranslation());
 
+        ImageView iconView = (ImageView) listItemView.findViewById(R.id.image);
+
+        if(currentWord.hasImage()) {
+            iconView.setImageResource(currentWord.getImageResourceId());
+            iconView.setVisibility(View.VISIBLE);
+        }
+        else{
+            iconView.setVisibility(View.GONE);
+        }
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        int color = ContextCompat.getColor(getContext(),backgroudColor);
+        textContainer.setBackgroundColor(color);
+
+        LinearLayout itemLayout = (LinearLayout) listItemView.findViewById(R.id.item_container);
+        mediaPlayer = MediaPlayer.create(getContext(), currentWord.getAudioResourceId());
+        // Set a click listener on that View
+        itemLayout.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers View is clicked on.
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+
         return listItemView;
     }
+
 }
